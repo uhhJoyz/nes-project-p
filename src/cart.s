ea4f1bca29ca7e1c9621166ede3a6937a7e08029
William Bradford <wcb8ze@virginia.edu>
2025-04-11 13:48:21 -0400

    progress toward loading sprites

--- src/cart.s
+++ src/cart.s
@@ -16,17 +16,86 @@ VAR:    .RES 1                  ; reserves 1 byte of memory in our "zero page" f
 
 .segment "STARTUP"
 RESET:
+    sei                         ; disable interrupts TODO: come back here and enable them later
+    cld                         ; disable decimal mode (unsupported by NES architecture)
+
+    ldx #$1000000                ; disable sound irq (load literal value 0x1000000 into index, or index register, X)
+    stx $4017                    ; store that value to the address (4017)
+    ldx #$00
+    stx $4010                   ; disable pcm
+
+    ;; initializing stack register
+    ldx #$FF                    ; load the literal 0xFF
+    txs                         ; transfer it to the stack register
+
+CLEAR_PPU:   
+    ;; clear the PPU (pixel processing unit) registers
+    lxd #$00
+    stx $2000
+    stx $2001
+
+: 
+    ;; wait for VBLANK (vertical blank)
+    bit $2002
+    bpl :-                      ; conditionally loop (branch if the result of the previous operation was positive)
+                                ; the :- here means jump to the previous anonymous symbol (defined 3 lines above with :)
+
+    txa
+CLEARMEMORY:
+    ;; this part is a loop that clears the memory of our device (takes a long time)
+    sta $0000, x
+    sta $0100, x
+    sta $0300, x
+    sta $0400, x
+    sta $0500, x
+    sta $0600, x
+    sta $0700, x
+
+    ;; we set the address 0x0200 to the literal 0xFF because this allows us to control when sprites are displayed
+    lda #$FF
+    sta $0200, x
+    lda #$00
+
+    inx
+    cpx #$00
+    bne CLEARMEMORY
+
+;; wait for vblank again
+:
+    bit $2002
+    bpl :-
+
+    lda #$02
+    sta $4014                   ; tells the PPU where the sprites are stored (8 kb range, so by setting this to 0x02,
+                                ; we tell the PPU that sprites are stored in addresses of the form 0x02__)
+    nop
+    lda #$3F                    ; PPU memory starts at 0x3F00, so we load the left 2 nybbles (most significant byte) into the A register
+    sta $2006                   
+    lda #$00
+    sta $2006
+    ;; what's happening here is that the PPU requires 2 writes to set its memory range properly - the first writes the most significant byte
+    ;; the second writes the least significant byte. This set of instructions tells the PPU that its memory starts at address 0x3F00
+
+    ldx #$00
+LOADPALETTES:
+    sta $2007
+    inx
+    cpx #$20
+    bne LOADPALETTES
+;; TODO: finish setting up sprite data and palette data
+
+
 INFLOOP:
-    jmp INFLOOP
+jmp INFLOOP
 
 NMI:
-    rti                         ; "return to interrupt" - basically if we get an NMI interrupt,
-                                ; ignore it and go back to doing what we were doing
+rti                         ; "return to interrupt" - basically if we get an NMI interrupt,
+                            ; ignore it and go back to doing what we were doing
 
 ;; this is a list of "handlers" for certain hardware events. it is essentially
 ;; all of the names for the "functions" we have written above
 .segment "VECTORS"
-    ;; declare the non-maskable interrupt so that we can tell when the monitor
+;; declare the non-maskable interrupt so that we can tell when the monitor
     ;; is in vertical blank state (when it is done being written to)
     .word NMI                   
     ;; allows us to control what happens when someone hits the reset button
